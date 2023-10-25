303e5678b159fcbd0d0d410676c75856af13a273
colin <colin.merk@gmx.ch>
2023-10-25 11:15:57 +1100

    change normalization

--- project2_template_codes/project2_test.py
+++ project2_template_codes/project2_test.py
@@ -133,7 +133,7 @@ logger.info(args)
 test_transform = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.ToTensor(), 
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
 ])
 
 ####################################
